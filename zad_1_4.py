cf384ad1d92d3c27ef1e5f108240fbd27ba8adb0
adamslawek <adam.slawek.1993@gmail.com>
2022-03-30 23:45:40 +0200

    Zadanie 1.4 | Opłata hotelowa

---
+++ zad_1_4.py
@@ -0,0 +1,20 @@
+wiek = int(input('Podaj swój wiek: '))
+ilosc_nocy = int(input('Podaj ile nocy spędzisz w hotelu: '))
+oplata = 200
+if wiek < 18:
+    oplata = 100
+elif ilosc_nocy >= 2 and ilosc_nocy < 5:
+    oplata = 180
+elif ilosc_nocy >= 5:
+    oplata = 150
+
+if wiek >= 65:
+    oplata = oplata * 0.9
+    if wiek >= 65 and ilosc_nocy >= 2 and ilosc_nocy < 5:
+        oplata = 180 * 0.9
+    elif wiek >= 65 and ilosc_nocy >= 5:
+        oplata = 150 * 0.9
+
+oplata_koncowa = oplata * ilosc_nocy
+
+print(oplata_koncowa, "PLN")
