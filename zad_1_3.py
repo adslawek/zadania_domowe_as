f4dda4f7dfa12cc8f2cb99ed2d32f972d85470bb
adamslawek <adam.slawek.1993@gmail.com>
2022-04-02 16:37:54 +0200

    Zadanie 1.3 BMI

---
+++ zad_1_3.py
@@ -0,0 +1,25 @@
+#BMI
+
+wzrost = float(input("Podaj swój wzrost w cm: "))
+masa = float(input("Podaj masę ciała w kg: "))
+BMI = 10000 * (masa / (wzrost ** 2))
+print(f'Twój współczynnik BMI wynosi {BMI:.2f}')
+
+if BMI < 16:
+    print("Twoje BMI wskazuje na wygłodzenie. Pilnie skontaktuj się z lekarzarzem!")
+elif BMI >= 16 and BMI <= 16.99:
+    print("Twoje BMI wskazuje na wychudzenie. Zalecamy pilny kontakt z lekarzem!")
+elif BMI >= 17 and BMI <= 18.49:
+    print("Cierpisz na niedowagę. Zalecamy konsultacje z lekarzem lub dietetykiem.")
+elif BMI >= 25 and BMI <= 29.99:
+    print("Cierpisz na nadwagę. Zalecamy konsultacje z lekarzem lub dietetykiem.")
+elif BMI >= 30 and BMI <= 34.99:
+    print("Twoje BMI wskazuje na I stopień otyłości. Zalecamy kontakt z lekarzem!")
+elif BMI >= 35 and BMI <= 39.99:
+    print("Twoje BMI wskazuje na I stopień otyłości. Zalecamy pilny kontakt z lekarzem!")
+elif BMI > 40:
+    print("Cierpisz na skrajną otyłość. Pilnie skontaktuj się z lekarzem!")
+else:
+    print("Wartość BMI jest w normie. Twoja waga jest prawidłowa!")
+
+print("Utrzymanie prawidłowej masy ciała jest ważne ze względu na lepsze samopoczucie oraz zdrowie i dłuższe życie.")
