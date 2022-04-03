2187e289a7ab428b8e3c30b3db88e785c8ad9f79
adamslawek <adam.slawek.1993@gmail.com>
2022-04-03 23:12:49 +0200

    Zadanie 2.2 | Choinka

---
+++ zad_2_2.py
@@ -0,0 +1,8 @@
+wysokosc = int(input("podaj wysokość choinki: "))
+a = 1
+while a < wysokosc + 1:
+    print(" " * (2*wysokosc - 2*a), " *"*a, "* "*(a-1))
+    a += 1
+
+#Matematykę skończyłem na poziomie szkoły średniej, to zadanie jest jednak matematycznie skomplikowane.
+#Przydałyby się wytłuamczenia jak to obliczyć, bo bez pomocy Mikołaja na pewno bym tego nie ogarnął.
