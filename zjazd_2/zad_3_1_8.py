f71709bcb65f41fd484ed9de8abe0ffdbe232409
adamslawek <adam.slawek.1993@gmail.com>
2022-04-14 21:19:59 +0200

    3.1.8 m na km

--- zjazd_2/zad_3_1_8.py
+++ zjazd_2/zad_3_1_8.py
@@ -2,11 +2,11 @@
 mile na km
 """
 
-def km_na_mile():
+def mile_na_km():
     mila = float(input("Podaj odleglosc w km: "))
     km = mila * 1.609344
     wynik = km
     print(f'{mila} m to {wynik:.2f} km.')
     return wynik
 
-km_na_mile()
+mile_na_km()
